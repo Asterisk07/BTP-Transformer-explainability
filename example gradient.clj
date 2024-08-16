a6242d458b120c446db0bc71b9069468f67372e7
Asterisk07 <ferbandphineas100@gmail.com>
2024-08-16 20:16:59 +0530

    changed from Sequential Batches (Full Pass) to Random Batches (Mini-Batch)

---
+++ example gradient.clj
@@ -0,0 +1,44 @@
+0   pos_embedding                             | [1, 325, 4] True
+1   cls_token                                 | [1, 1, 4] True
+2   patch_embedding.projection.1.weight       | [4, 64] True
+3   patch_embedding.projection.1.bias         | [4] True
+4   layers.0.0.fn.norm.weight                 | [4] True
+5   layers.0.0.fn.norm.bias                   | [4] True
+6   layers.0.0.fn.fn.att.in_proj_weight       | [12, 4] True
+7   layers.0.0.fn.fn.att.in_proj_bias         | [12] True
+8   layers.0.0.fn.fn.att.out_proj.weight      | [4, 4] True
+9   layers.0.0.fn.fn.att.out_proj.bias        | [4] True
+10  layers.0.0.fn.fn.q.weight                 | [4, 4] False
+11  layers.0.0.fn.fn.q.bias                   | [4] False
+12  layers.0.0.fn.fn.k.weight                 | [4, 4] False
+13  layers.0.0.fn.fn.k.bias                   | [4] False
+14  layers.0.0.fn.fn.v.weight                 | [4, 4] False
+15  layers.0.0.fn.fn.v.bias                   | [4] False
+16  layers.0.1.fn.norm.weight                 | [4] True
+17  layers.0.1.fn.norm.bias                   | [4] True
+18  layers.0.1.fn.fn.0.weight                 | [4, 4] True
+19  layers.0.1.fn.fn.0.bias                   | [4] True
+20  layers.0.1.fn.fn.3.weight                 | [4, 4] True
+21  layers.0.1.fn.fn.3.bias                   | [4] True
+22  layers.1.0.fn.norm.weight                 | [4] True
+23  layers.1.0.fn.norm.bias                   | [4] True
+24  layers.1.0.fn.fn.att.in_proj_weight       | [12, 4] True
+25  layers.1.0.fn.fn.att.in_proj_bias         | [12] True
+26  layers.1.0.fn.fn.att.out_proj.weight      | [4, 4] True
+27  layers.1.0.fn.fn.att.out_proj.bias        | [4] True
+28  layers.1.0.fn.fn.q.weight                 | [4, 4] False
+29  layers.1.0.fn.fn.q.bias                   | [4] False
+30  layers.1.0.fn.fn.k.weight                 | [4, 4] False
+31  layers.1.0.fn.fn.k.bias                   | [4] False
+32  layers.1.0.fn.fn.v.weight                 | [4, 4] False
+33  layers.1.0.fn.fn.v.bias                   | [4] False
+34  layers.1.1.fn.norm.weight                 | [4] True
+35  layers.1.1.fn.norm.bias                   | [4] True
+36  layers.1.1.fn.fn.0.weight                 | [4, 4] True
+37  layers.1.1.fn.fn.0.bias                   | [4] True
+38  layers.1.1.fn.fn.3.weight                 | [4, 4] True
+39  layers.1.1.fn.fn.3.bias                   | [4] True
+40  head.0.weight                             | [4] True
+41  head.0.bias                               | [4] True
+42  head.1.weight                             | [1, 4] True
+43  head.1.bias                               | [1] True
